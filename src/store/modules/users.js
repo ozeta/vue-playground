ddf320d5eecea14dc9a2e42218cc8bac1967ab24
Marco Carrozzo <ozeta86@gmail.com>
2020-05-23 17:36:30 +0200

    rest is working

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -4,29 +4,7 @@ const users_module = {
   namespaced: true,
   state: {
     new_user: '',
-    users: [{
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-          "lat": "-37.3159",
-          "lng": "81.1496"
-        }
-      },
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org",
-      "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-      }
-    }]
+    users: []
   },
   mutations: {
     ADD_USER(state, data) {
@@ -52,7 +30,6 @@ const users_module = {
       if (!state.new_user) {
         return
       }
-
       const user = {
         id: 333,
         name: "Marco",
@@ -65,7 +42,7 @@ const users_module = {
   },
   getters: {
     new_user: state => state.new_user,
-    userss: state => state.users,
+    users: state => state.users,
     filtered_users(state) {
       console.log("%cGetter invoked", "font-weight: bold;color:white; background: blue")
       return state.users.filter((obj) => obj.id > 3)
